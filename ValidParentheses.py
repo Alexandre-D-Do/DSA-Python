0c2dd83b16cf7a23c2dc4363de7c1e9a05adb03c
Aevael <98851230+Aevael@users.noreply.github.com>
2025-11-05 23:21:20 -0600

    Valid Parentheses work in progress upload.

---
+++ ValidParentheses.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        slen = len(s)
+        for i, char in enumerate(s) :
+            
+            if (i % 2) == 0 :
+                # Check if this is the last character
+                if i == (slen - 1) :
+                    return False
+                elif s[i: i+1] == "(" :
+                    if s[i+1: i+1] != ")" :
+                        return False
+                elif s[i: i+1] == "[" :
+                    if s[i+1: i+1] != "]" :
+                        return False
+                elif s[i: i+1] == "{" :
+                    if s[i+1: i+1] != "}" :
+                        return False
+                else :
+                    return False
+        return True
