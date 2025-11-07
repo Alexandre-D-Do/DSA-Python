002f173d958bd9575a16f426d9d0f18f9383eda8
Aevael <98851230+Aevael@users.noreply.github.com>
2025-11-07 13:56:28 -0600

    Valid Parentheses Solution upload.

--- ValidParentheses.py
+++ ValidParentheses.py
@@ -1,24 +1,32 @@
+# The runtime complexity of this solution is O(N)
 class Solution:
     def isValid(self, s: str) -> bool:
-        slen = len(s)
-        for i, char in enumerate(s) :
-            
-            if (i % 2) == 0 :
-                # Check if this is the last character
-                if i == (slen - 1) :
-                    return False
-                elif s[i: i+1] == "(" :
-                    if s[i+1: i+1] != ")" :
-                        return False
-                elif s[i: i+1] == "[" :
-                    if s[i+1: i+1] != "]" :
-                        return False
-                elif s[i: i+1] == "{" :
-                    if s[i+1: i+1] != "}" :
-                        return False
-                else :
-                    return False
-        return True
+        stack = []
+        dictionary = {'(' : ')', '[' : ']', '{' : '}' }
+        if (len(s) % 2) != 0:
+            return False
+        for char in s:
+            if char in dictionary:
+                stack.append(char)
+            elif not stack or dictionary[stack.pop()] != char:
+                return False
+        if stack:
+            return False
+        else: 
+            return True
 
 
-''''''
+'''
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+    Open brackets must be closed by the same type of brackets.
+    Open brackets must be closed in the correct order.
+    Every close bracket has a corresponding open bracket of the same type.
+Constraints:
+
+    1 <= s.length <= 104
+    s consists of parentheses only '()[]{}'.
+
+'''
