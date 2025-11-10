8a07bcd62c247150df1d774df8c4abebd5a6233f
Aevael <98851230+Aevael@users.noreply.github.com>
2025-11-09 18:22:33 -0600

    Longest Palindromic Substring solution update.

--- LongestPalindromicSubstring.py
+++ LongestPalindromicSubstring.py
@@ -1,26 +1,48 @@
+#The runtime complexity of this solution is O(N^2)
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        reverseds = s[::-1]
-        slength = len(s) 
-        longestlength = 1
-        longestsub = s[0]
-        # Check if s is a palindrome
-        if s == reverseds or slength == 1:
-            return s
-        for i, char in enumerate(s):
-            slength = len(s)
-            if i == (len(s) - 1):
-                break
-            substring = s[i:]
-            reversedsub = substring[::-1]
-            if substring == reversedsub and len(substring) > longestlength:
-                    longestlength = len(substring)
-                    longestsub = substring
-            while slength > i:
-                substring = s[i : slength - 1]
-                reversedsub = substring[::-1]
-                if substring == reversedsub and len(substring) > longestlength:
-                    longestlength = len(substring)
-                    longestsub = substring
-                slength -= 1 
-        return longestsub
+        size = len(s)
+        start = 0
+        end = 0
+
+        for i in range(size):
+            for j in range(2):
+                left = i
+                right = i + j
+                while left >=0 and right + 1 <= size and s[left] == s[right]:
+                    left -= 1
+                    right += 1
+                left += 1
+                right -= 1
+                if (right - left) > (end - start):
+                    end = right
+                    start = left
+                    
+        return s[start: end + 1]
+
+'''
+Given a string s, return the longest
+
+in s.
+
+ 
+
+Example 1:
+
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+
+Example 2:
+
+Input: s = "cbbd"
+Output: "bb"
+
+ 
+
+Constraints:
+
+    1 <= s.length <= 1000
+    s consist of only digits and English letters.
+
+'''
